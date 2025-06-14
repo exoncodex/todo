c5e0aa69b61fa7fdf0e9999048ff291219282bfe
exoncodex <exoncodex@gmail.com>
2025-06-15 02:28:52 +0600

    working register

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate,logout,login
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -8,6 +9,19 @@ def home(request):
 def loginpage(request):
   return render(request,"login.html")
 def register(request):
+  if request.method == "POST":
+    username=request.POST.get("username")
+    password=request.POST.get("password")
+    email=request.POST.get("email")
+    if User.objects.filter(username=username,email=email):
+      messages.error(request,"User already Exist")
+    else:
+      ussave=User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+        ) 
+      ussave.save()
   return render(request,"register.html")
 def logoutpage(request):
   logout(request)
